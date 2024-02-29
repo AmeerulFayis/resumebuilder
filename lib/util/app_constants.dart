7eea2982127ca6debde599f014d66187036fd6e1
AmeerulFayis <Ameerulfayis@gmail.com>
2024-02-29 12:01:58 +0530

    added app constants page

---
+++ lib/util/app_constants.dart
@@ -0,0 +1,39 @@
+// screen sizes
+import 'package:flutter/cupertino.dart';
+
+double screenWidth(context) {
+  return MediaQuery.of(context).size.width;
+}
+
+double screenHeight(context) {
+  return MediaQuery.of(context).size.height;
+}
+
+
+// divider
+
+Widget dividerH20() {
+  return const SizedBox(height: 20);
+}
+
+Widget dividerH10() {
+  return const SizedBox(height: 10);
+}
+
+Widget dividerH5() {
+  return const SizedBox(height: 5);
+}
+
+
+Widget dividerW10() {
+  return const SizedBox(width: 10);
+}
+
+Widget dividerW5() {
+  return const SizedBox(width: 5);
+}
+
+Widget dividerW20() {
+  return const SizedBox(width: 20);
+}
+
