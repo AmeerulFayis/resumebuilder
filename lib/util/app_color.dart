d64279a8d31f7bfb3907c9647303b37f352e7df3
AmeerulFayis <Ameerulfayis@gmail.com>
2024-02-29 11:57:53 +0530

    added constant colors

---
+++ lib/util/app_color.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColor{
+static const kGreen=Color(0xff99D400);
+static const kDarkGreen=Color(0xff668c01);
+static const kBlack=Color(0xff000000);
+static const kWhite=Color(0xffffffff);
+}
