2b72b35633ad42039248bb9462560e6049ba665c
AmeerulFayis <Ameerulfayis@gmail.com>
2024-02-29 14:22:54 +0530

    updated data model.dart and generated data model g dart file

--- lib/model/data_model.dart
+++ lib/model/data_model.dart
@@ -1,9 +1,27 @@
+
+
+
+import 'package:hive/hive.dart';
+part 'data_model.g.dart';
+
+@HiveType(typeId: 1)
 class ResumeModel{
+  @HiveField(0)
   final String name;
+
+  @HiveField(1)
   final String jobRole;
+
+  @HiveField(2)
   final String email;
+
+  @HiveField(3)
   final String phoneNumber;
+
+  @HiveField(4)
   final String linkedin;
+
+  @HiveField(5)
   final String summary;
   ResumeModel(
       {
---
+++ lib/model/data_model.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ResumeModelAdapter extends TypeAdapter<ResumeModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ResumeModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ResumeModel(
+      name: fields[0] as String,
+      jobRole: fields[1] as String,
+      email: fields[2] as String,
+      phoneNumber: fields[3] as String,
+      linkedin: fields[4] as String,
+      summary: fields[5] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ResumeModel obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.jobRole)
+      ..writeByte(2)
+      ..write(obj.email)
+      ..writeByte(3)
+      ..write(obj.phoneNumber)
+      ..writeByte(4)
+      ..write(obj.linkedin)
+      ..writeByte(5)
+      ..write(obj.summary);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ResumeModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
