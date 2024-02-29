8970c1ae5c75ad4f8a481bc22b51ebd0b22dc8cf
AmeerulFayis <Ameerulfayis@gmail.com>
2024-02-29 12:18:05 +0530

    home page added, navigation to the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resumebuilder/ui/screens/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: HomePage(),
     );
   }
 }
